39317df223327259ecbb491463d8b5f3982b4540
gabrielcapilla <104425110+gabrielcapilla@users.noreply.github.com>
2023-11-15 02:33:36 +0100

    Update aplicar.sh

--- aplicar.sh
+++ aplicar.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-i="./themes *.yml" o=$HOME/.config/alacritty/; cp -r $i "$o"
+# Instalar tipografía
+mkdir -p ~/.local/share/fonts/fragment
+git clone https://github.com/weiweihuanghuang/fragment-mono.git
+cp -u -r fragment-mono/fonts/otf/* ~/.local/share/fonts/fragment
+rm -rf fragment-mono
+
+# Instalar configuración Alacritty
+mkdir -p ~/.config/alacritty/; cd alacritty
+i="./themes *.yml" o=~/.config/alacritty/; cp -r $i "$o"
 
 
